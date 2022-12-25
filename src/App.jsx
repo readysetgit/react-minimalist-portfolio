1973e7d041643e33f51a62941f6214ef07dc0eee
Fahad <fahadfile@gmail.com>
2022-12-25 21:28:05 +0300

    class -> className

--- src/App.jsx
+++ src/App.jsx
@@ -21,117 +21,116 @@ function App() {
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <link rel="stylesheet"
         href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    
-    <div class="container">
-        <div class="section-container fun-container">
-            <h1 class="title-font">
+    <div className="container">
+        <div className="section-container fun-container">
+            <h1 className="title-font">
                 Sports
             </h1>
-            <p class="description">
+            <p className="description">
                 A sound mind in a sound body
             </p>
-            <div class="sports-container">
-                <div class="sport">
-                    <p class="name"><span class="material-symbols-outlined">
+            <div className="sports-container">
+                <div className="sport">
+                    <p className="name"><span className="material-symbols-outlined">
                             sports_tennis
                         </span></p>
-                    <span class="skill-level">
-                        <span class="skill-bar active"></span>
-                        <span class="skill-bar active"></span>
-                        <span class="skill-bar active"></span>
-                        <span class="skill-bar active"></span>
-                        <span class="skill-bar"></span>
+                    <span className="skill-level">
+                        <span className="skill-bar active"></span>
+                        <span className="skill-bar active"></span>
+                        <span className="skill-bar active"></span>
+                        <span className="skill-bar active"></span>
+                        <span className="skill-bar"></span>
                     </span>
                 </div>
-                <div class="sport expanded">
-                    <p class="name"><span class="material-symbols-outlined">
+                <div className="sport expanded">
+                    <p className="name"><span className="material-symbols-outlined">
                             sports_tennis
                         </span></p>
-                    <span class="skill-level">
-                        <span class="skill-bar active"></span>
-                        <span class="skill-bar active"></span>
-                        <span class="skill-bar active"></span>
-                        <span class="skill-bar active"></span>
-                        <span class="skill-bar"></span>
+                    <span className="skill-level">
+                        <span className="skill-bar active"></span>
+                        <span className="skill-bar active"></span>
+                        <span className="skill-bar active"></span>
+                        <span className="skill-bar active"></span>
+                        <span className="skill-bar"></span>
                     </span>
                 </div>
-                <div class="sport">
-                    <p class="name"><span class="material-symbols-outlined">
+                <div className="sport">
+                    <p className="name"><span className="material-symbols-outlined">
                             adjust
                         </span></p>
-                    <span class="skill-level">
-                        <span class="skill-bar active"></span>
-                        <span class="skill-bar active"></span>
-                        <span class="skill-bar active"></span>
-                        <span class="skill-bar "></span>
-                        <span class="skill-bar"></span>
+                    <span className="skill-level">
+                        <span className="skill-bar active"></span>
+                        <span className="skill-bar active"></span>
+                        <span className="skill-bar active"></span>
+                        <span className="skill-bar "></span>
+                        <span className="skill-bar"></span>
                     </span>
                 </div>
-                <div class="sport">
-                    <p class="name"><span class="material-symbols-outlined">
+                <div className="sport">
+                    <p className="name"><span className="material-symbols-outlined">
                             sports_rugby
                         </span></p>
-                    <span class="skill-level">
-                        <span class="skill-bar active"></span>
-                        <span class="skill-bar active"></span>
-                        <span class="skill-bar "></span>
-                        <span class="skill-bar "></span>
-                        <span class="skill-bar"></span>
+                    <span className="skill-level">
+                        <span className="skill-bar active"></span>
+                        <span className="skill-bar active"></span>
+                        <span className="skill-bar "></span>
+                        <span className="skill-bar "></span>
+                        <span className="skill-bar"></span>
                     </span>
                 </div>
-                <div class="sport">
-                    <p class="name"><span class="material-symbols-outlined">
+                <div className="sport">
+                    <p className="name"><span className="material-symbols-outlined">
                             landscape
                         </span></p>
-                    <span class="skill-level">
-                        <span class="skill-bar active"></span>
-                        <span class="skill-bar "></span>
-                        <span class="skill-bar "></span>
-                        <span class="skill-bar "></span>
-                        <span class="skill-bar"></span>
+                    <span className="skill-level">
+                        <span className="skill-bar active"></span>
+                        <span className="skill-bar "></span>
+                        <span className="skill-bar "></span>
+                        <span className="skill-bar "></span>
+                        <span className="skill-bar"></span>
                     </span>
                 </div>
             </div>
         </div>
-        <div class="section-container xp-container">
-            <h1 class="title-font">
+        <div className="section-container xp-container">
+            <h1 className="title-font">
                 Experience
             </h1>
-            <p class="description">
+            <p className="description">
                 From fixing my grandmother's WiFi to working in startups, I have acquired a myriad of technical skills and
                 used numerous programming languages. Here are some companies I've worked with:
             </p>
-            <div class="square-container">
-                <div class="company-square">
-                    <h2 class="title">
+            <div className="square-container">
+                <div className="company-square">
+                    <h2 className="title">
                         The Normies
                     </h2>
-                    <p class="metadata">
-                        <span class="material-symbols-outlined">
+                    <p className="metadata">
+                        <span className="material-symbols-outlined">
                             schedule
                         </span>
                         5 months
                     </p>
-                    <p class="metadata">
-                        <span class="material-symbols-outlined">
+                    <p className="metadata">
+                        <span className="material-symbols-outlined">
                             calendar_month
                         </span>
                         08/2022 - 12/2022
                     </p>
     
                 </div>
-                <div class="company-square">
-                    <h2 class="title">
+                <div className="company-square">
+                    <h2 className="title">
                         The Normies
                     </h2>
-                    <p class="metadata">
-                        <span class="material-symbols-outlined">
+                    <p className="metadata">
+                        <span className="material-symbols-outlined">
                             schedule
                         </span>
                         5 months
                     </p>
-                    <p class="metadata">
-                        <span class="material-symbols-outlined">
+                    <p className="metadata">
+                        <span className="material-symbols-outlined">
                             calendar_month
                         </span>
                         08/2022 - 12/2022
